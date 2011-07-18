a5ec1092aafe44378b16cd9384c8fb56d50bf5a3
gothroach <github@0xf.org>
2011-07-18 15:27:26 -0700

    Bare shell of new class to hold problem tickets.

---
+++ src/main/java/org/zeroxf/Problem/ProblemTicket.java
@@ -0,0 +1,91 @@
+package org.zeroxf.Problem;
+
+/**
+ * Data class to hold a problem ticket and related information.
+ * 
+ * @author necavi
+ * @author gothroach
+ */
+public class ProblemTicket {
+    private String owner;
+    private String assignee;
+    private int time;
+    private String status;
+    private ProblemComment[] comments;
+    
+    public ProblemTicket()
+    {
+        this.owner = null;
+        this.assignee = null;
+        this.time = -1;
+        this.status = null;
+        this.comments = new ProblemComment[32];
+    }
+    
+    public ProblemTicket(String owner, String assignee, int time, String status, ProblemComment[] comments)
+    {
+        this.setOwner(owner);
+        this.setAssignee(assignee);
+        this.setTime(time);
+        this.setStatus(status);
+        this.comments = new ProblemComment[32];
+        this.setComments(comments);
+    }
+    
+    protected String getOwner()
+    {
+        return this.owner;
+    }
+    protected final void setOwner(String owner)
+    {
+        this.owner = owner;
+    }
+    
+    protected String getAssignee()
+    {
+        return this.assignee;
+    }
+    protected final void setAssignee(String assignee)
+    {
+        this.assignee = assignee;
+    }
+    
+    protected int getTime()
+    {
+        return this.time;
+    }
+    protected final void setTime(int time)
+    {
+        this.time = time;
+    }
+    
+    protected String getStatus()
+    {
+        return this.status;
+    }
+    protected final void setStatus(String status)
+    {
+        this.status = status;
+    }
+    
+    protected ProblemComment[] getComments()
+    {
+        return this.comments;
+    }
+    protected ProblemComment getComment(int i)
+    {
+        if (i < this.comments.length && i >= 0)
+            return this.comments[i];
+        else
+            return null;
+    }
+    protected final void setComments(ProblemComment[] comments)
+    {
+        this.comments = comments;
+    }
+    protected final void setComment(ProblemComment comment, int i)
+    {
+        if (i >=0)
+            this.comments[i] = comment;
+    }
+}
